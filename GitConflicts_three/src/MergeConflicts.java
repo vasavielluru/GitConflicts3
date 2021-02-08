eee7f2dbf1e0974d20d0f97c0162a81e689cd22c
vasavi elluru <elluruvasavi.19993@gmail.com>
2021-02-08 11:39:48 +0530

    Commiting after adding a statement

--- GitConflicts_three/src/MergeConflicts.java
+++ GitConflicts_three/src/MergeConflicts.java
@@ -3,6 +3,7 @@ public class MergeConflicts {
 
 	public static void main(String[] args) {
 		System.out.println("The conflicts are yet to be solved");
+		System.out.println("The conflicts are solved through eclipse");
 
 	}
 
